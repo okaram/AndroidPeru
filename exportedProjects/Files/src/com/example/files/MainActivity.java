0a1f0b5b8ba226ad1b76c4002afc0b1962d7fc56
Orlando Karam <orlando.karam@gmail.com>
2013-05-14 12:12:49 -0400

    misc

--- exportedProjects/Files/src/com/example/files/MainActivity.java
+++ exportedProjects/Files/src/com/example/files/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.files;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -33,7 +39,7 @@ public class MainActivity extends Activity {
 		StringBuffer b=new StringBuffer();
 		String line;
 		try {
-			while((line=inr.readLine())!=null) {
+			while((line=inr.readLine())!=null) { 
 				b.append(line);
 				b.append("\n");
 			}
@@ -41,6 +47,20 @@ public class MainActivity extends Activity {
 		
 		}
 		return b.toString();
+	}
+	
+	void writeToFile(String s, OutputStream o1)
+	{
+		Log.d("ttt","writing "+s);
+		Writer o2= new BufferedWriter(new OutputStreamWriter(o1));
+		try {
+			o2.write(s,0,s.length());
+			o2.close();
+		} catch (IOException e){
+			Log.e("ttt","Error writing");
+		} finally {
+		}
+		
 	}
 	
 	public void readRaw(View v)
@@ -58,6 +78,39 @@ public class MainActivity extends Activity {
 			tv.setText(readFile(in));
 		} catch (IOException e) {
 			Log.e("ttt","Couldn't find the file");
+		}
+	}
+
+	public void readLocal(View v)
+	{
+		try {
+			InputStream in=openFileInput("test.txt");
+			TextView tv=(TextView)findViewById(R.id.textView1);
+			tv.setText(readFile(in));
+		} catch (IOException e) {
+			Log.e("ttt","Couldn't find the file");
+		}
+	}
+	
+	public void writeLocal(View v)
+	{
+		try {
+			OutputStream out=openFileOutput("test.txt", 0);
+			TextView tv=(TextView)findViewById(R.id.textView1);
+			writeToFile(tv.getText().toString(), out);
+		} catch (Exception e) {
+			Log.e("ttt","Couldn't write local test.txt");
+		}
+	}
+	
+	public void readLocalFolder(View v)
+	{
+		try {
+			File in1=new File(getFilesDir(),"test.txt");
+			TextView tv=(TextView)findViewById(R.id.textView1);
+			tv.setText(readFile(new FileInputStream(in1)));
+		} catch (Exception e) {
+			Log.e("ttt","couldn't find it");
 		}
 	}
 	
